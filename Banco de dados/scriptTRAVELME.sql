18351cfa626c123efeb6771a8f56811d9f4f918d
CaueGhelfi03 <caueghelfi@sptech.school>
2023-11-22 22:29:44 -0300

    Inserindo backend do endereco

--- Banco de dados/scriptTRAVELME.sql
+++ Banco de dados/scriptTRAVELME.sql
@@ -15,14 +15,17 @@ create table endereco(
 	idEndereco int primary key auto_increment,
     rua varchar(90),
     bairro varchar(45),
+    unidadeFederativa varchar(2),
     cidade varchar(90),
     CEP varchar(8),
+    complemento varchar(20),
 	fkUsuario int,
     constraint foreign key (fkUsuario) references usuario (idUsuario));
     
 create table mensagem(
-	idMensagem int,
+	idMensagem int auto_increment,
 	fkUsuario int,
+    titulo varchar (90),
 	descricao varchar(255),
 	avaliacao int,
     localViagem varchar(100),
@@ -32,9 +35,10 @@ create table mensagem(
     primary key (idMensagem,fkUsuario));
     
     
-
 	INSERT INTO usuario (nome, email, senha, cpf,telefone) VALUES ('Caue Ghelfi', 'caueghelfi@sptech.school', 'ghelfinho', '12345678912', '11971248838');
     select * from usuario;
+    select * from mensagem;
+    select * from endereco;
     
     
     
